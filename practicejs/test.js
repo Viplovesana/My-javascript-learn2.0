edbf359a77f3d2b45ebc50ce4384026c02c8966a
Viplovesana <viplovesana90@gmail.com>
2026-01-09 23:01:20 +0530

    new commit

--- practicejs/test.js
+++ practicejs/test.js
@@ -4,3 +4,25 @@
 // console.log(3+3)
 // console.log("viplove")
 
+// var x = "viplove";
+// console.log(x);// not hoisted
+
+
+// console.log(x);
+// var x = "viplove";
+
+// for (var i=0;i<3;i++){
+//     console.log("inside",i)//var doesnot support for 
+// }
+//  console.log("inside",i);
+
+// for (let i=0;i<3;i++){
+//     console.log("inside",i)//let is block scope thats why its only exicute inside the block
+// }
+//  console.log("inside",i);//thorugh error
+ 
+// for (const i=0;i<3;i++){
+//     console.log("inside",i)// it cannot change or itrate the value because its constant
+// }
+//  console.log("inside",i); 
+ 
