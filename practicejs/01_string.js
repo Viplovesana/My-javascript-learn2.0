19d2d085c75c83e55ce639bde9965ce57f155e79
Viplovesana <viplovesana90@gmail.com>
2026-01-13 08:39:50 +0530

    new commit

--- practicejs/01_string.js
+++ practicejs/01_string.js
@@ -10,13 +10,21 @@ let age = "23"
 console.log(`My name is ${name} and I am ${age} years old`)
 
 const gameName = new String("Viplove")
+// const gameName = new String("Viplove-sana-Hero")
 console.log(gameName.substring(0,2))
+
 console.log(gameName [0])
+
 console.log(typeof gameName)
+
 console.log(gameName.__proto__);
+
 console.log(gameName.length);
+
 console.log(gameName.indexOf('v'));
+
 console.log(gameName.charAt(4));
+
 console.log(gameName.toUpperCase());
 
 const newString = gameName.substring(0,4)
@@ -31,3 +39,7 @@ console.log(newString1.trim())
 
 const uRl = "https://viplove.come/viplove%20sana";
 console.log(uRl.replace('%20','-'))
+
+console.log(uRl.includes("sana"))
+
+console.log(gameName.split("-"))
