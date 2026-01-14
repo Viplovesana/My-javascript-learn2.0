4197dcaf20930d6f6531d48b76b4efe72678c0e8
Viplovesana <viplovesana90@gmail.com>
2026-01-14 22:23:08 +0530

    array-concept

---
+++ 02_basics/02_array.js
@@ -0,0 +1,7 @@
+//Array.....................
+
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc_heros = ["Flash","superman","batman"]
+
+marvel_heros.push(dc_heros)
+console.log(marvel_heros)
