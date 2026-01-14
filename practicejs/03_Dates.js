ad33db3c05f66906c6f00d3dffd40a4176cb8c17
Viplovesana <viplovesana90@gmail.com>
2026-01-14 08:45:36 +0530

    DateANDtime

--- practicejs/03_Dates.js
+++ practicejs/03_Dates.js
@@ -1,2 +1,28 @@
     
 
+
+// let myDate = new Date()
+// console.log(myDate.toString())    
+// console.log(myDate.toDateString())    
+// console.log(myDate.toLocaleDateString())    
+// console.log(typeof myDate)  
+
+// let createMyDate = new Date(2026 , 0, 24 , 5, 30)
+// let createMyDate = new Date("2026-01-14")
+// console.log(createMyDate.toLocaleString())
+
+// let myTimeStamp = Date.now()++
+
+// console.log(myTimeStamp)
+// console.log(createMyDate.getTime())
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getMonth()+1)
+console.log(newDate.getDay())
+
+newDate.toLocaleString('default'{
+    weekday: "long",
+})
+
+
