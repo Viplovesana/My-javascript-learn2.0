8e3b4772e672ac0e93ac37745ba67b19b88353b9
Viplovesana <viplovesana90@gmail.com>
2026-01-26 19:04:16 +0530

    switch-case

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -7,18 +7,36 @@
 //         break;
 // }
 
-const month = 1
-switch (month) {
-    case 1:
-        console.log("january");
+// const month = 1
+// switch (month) {
+//     case 1:
+//         console.log("january"); //if it match the case 1: and comment the break ten its excecute the case 2:aswell
+//         // break;
+//     case 2:
+//         console.log("feburrary");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     default:
+//         console.log("default value match"); 
+//         break;
+// }
+const months = "feb"
+switch (months) {
+    case "jan":
+        console.log("january"); //if it match the case 1: and comment the break ten its excecute the case 2:aswell
         // break;
-    case 2:
+    case "feb":
         console.log("feburrary");
         break;
-    case 3:
+    case "march":
         console.log("march");
         break;
-    case 4:
+    case "april":
         console.log("april");
         break;
     default:
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,44 @@
+// const userEmail = "viplove@123" // have a true value in the string
+
+// if (userEmail) {
+//     console.log("Got user Email");
+// }else{
+//     console.log("dont, have user Email");   
+
+// }
+const userEmail = "" // have the flase value string is empty 
+
+if (userEmail) {
+    console.log("Got user Email");
+}else{
+    console.log("dont, have user Email");   
+}
+
+// falsy values ......
+
+// false , 0, -0, BigInt 0n, "", null, undefined, NAN 
+
+// truthy values .......
+
+// "0", 'false', " ", [], {}, function(){}
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+const checkObject = {}
+if (Object.keys(checkObject).length === 0) {
+    console.log("Object is Empty");  
+}
+
+// nullish Coalescing Operator (??):-null, undefined.....................
+
+let val1;
+// val1 = 5 ?? 10
+val1 = null ?? 10
+console.log(val1);
+
+
+
+
+
